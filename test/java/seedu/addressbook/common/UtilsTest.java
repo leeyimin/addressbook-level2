8d5f04caa427cea87e4dac799695389c3c0c120e
leeyimin <imleeyimin@gmail.com>
2016-09-07 02:03:08 +0800

    Test elementsAreUnique

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -2,6 +2,9 @@ package seedu.addressbook.common;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class UtilsTest {
@@ -25,6 +28,24 @@ public class UtilsTest {
 		assertTrue(Utils.isAnyNull(new Object[]{1,2,3,null,5,6}));
 		//chain with one null object
 		assertTrue(Utils.isAnyNull(1,2,3,4,5,6,7,null,8));
+		
+	}
+	
+	@Test
+	public void testElementsAreUnique(){
+		//empty collection
+		assertTrue(Utils.elementsAreUnique(new ArrayList()));
+		//unique characters
+		ArrayList<Character> list = new ArrayList<>();
+		for(char x: "Testing".toCharArray()){
+			list.add(x);
+		}
+		assertTrue(Utils.elementsAreUnique(list));
+		//repeating characters
+		for(char x: " eww".toCharArray()){
+			list.add(x);
+		}
+		assertFalse(Utils.elementsAreUnique(list));
 		
 	}
 
