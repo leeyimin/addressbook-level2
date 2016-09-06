88586b8a0ed6324e9a0c46a94c3d3cc41699d74f
leeyimin <imleeyimin@gmail.com>
2016-09-07 02:13:00 +0800

    Add NameTest

---
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -0,0 +1,23 @@
+package seedu.addressbook.data.person;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.tag.UniqueTagList;
+
+public class NameTest {
+
+	@Test
+	public void testIsSimilar() {
+		try {
+			Name name = new Name("John K Smith");
+			assertTrue(name.isSimilar("John K Smith"));
+		} catch (IllegalValueException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
