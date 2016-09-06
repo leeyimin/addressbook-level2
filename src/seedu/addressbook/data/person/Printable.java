de1f799bea3e611fe86cdaecac78f44e9d36d13d
leeyimin <imleeyimin@gmail.com>
2016-09-07 00:40:19 +0800

    Add getPrintableString(Printable... printables) in Printable

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -4,4 +4,16 @@ public interface Printable {
 	
 	public String getPrintableString();
 
+	/**
+	  * Returns a concatenated version of the printable strings of each object.
+	  */
+	public static String getPrintableString(Printable... printables){
+		StringBuilder builder = new StringBuilder();
+		builder.append(printables[0].getPrintableString());
+		for(int i=1;i<printables.length;i++){
+			builder.append(", ");
+			builder.append(printables[i].getPrintableString());
+		}
+		return builder.toString();
+	}
 }
